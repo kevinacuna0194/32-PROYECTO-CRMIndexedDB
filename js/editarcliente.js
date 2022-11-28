570f415bebb952cf6e3c8a41cca181d2d22bc71e
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2022-11-27 23:56:35 -0300

    Actualizar registros en la BD

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,18 +1,27 @@
 (function () {
 
     let DB;
+    let idCliente;
 
     const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+
+    const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
 
         conectarDB();
 
+        /** Actualizar registro */
+        formulario.addEventListener('submit', actualizarCliente);
+
         /** Verificar el ID de la URL 
          * Primero va a leer la URL, query string; Así se le conoce y en caso de que haya uno, va a mandar llamar a esa función de obtener cliente.
         */
         const parametrosURL = new URLSearchParams(window.location.search);
-        const idCliente = parametrosURL.get('id');
+        idCliente = parametrosURL.get('id');
         // console.log(idCliente);
 
         if (idCliente) {
@@ -22,6 +31,45 @@
             }, 100);
         }
     });
+
+    function actualizarCliente(e) {
+        e.preventDefault();
+
+        // Validar
+        if (nombreInput.value === '' || emailInput.value === '' || telefonoInput.value === '' || empresaInput.value === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+            return;
+        }
+
+        /** Si se pasa esta validación tendríamos que actualizar el cliente */
+        const clienteActualizado = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: nombreInput.value,
+            id: Number(idCliente)
+        };
+
+        // console.log(clienteActualizado);
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        /** Esto lo que va a hacer es encontrar el ID y como nuestra BD le definimos keypath como el ID, entonces va a verlos que son iguales y va a actualizar con los nuevos datos. */
+        objectStore.put(clienteActualizado);
+
+        transaction.oncomplete = function () {
+            imprimirAlerta('Editado correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+
+        transaction.onerror = function () {
+            imprimirAlerta('Hubo un error', 'error');
+        }
+    }
 
     function obtenerCliente(id) {
         /** Para obtener el cliente necesitamos conectarnos a la base de datos. */
@@ -35,7 +83,7 @@
             if (cursor) {
                 // console.log(cursor.value);
                 if (cursor.value.id === Number(id)) {
-    
+
                     llenarFormulario(cursor.value);
                 }
 
@@ -45,9 +93,12 @@
     }
 
     function llenarFormulario(datosCliente) {
-        const { nombre } = datosCliente;
+        const { nombre, email, telefono, empresa } = datosCliente;
 
         nombreInput.value = nombre;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        empresaInput.value = empresa;
     }
 
     function conectarDB() {
--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -11,20 +11,6 @@
         formulario.addEventListener('submit', validarCliente);
     });
 
-    function conectarDB() {
-
-        const abrirConexion = window.indexedDB.open('crm', 1);
-
-        abrirConexion.onerror = function () {
-            console.log('Hubo un error');
-        }
-
-        abrirConexion.onsuccess = function () {
-            /** Instancia a la BD con todos los métodos disponibles */
-            DB = abrirConexion.result;
-        }
-    }
-
     function validarCliente(e) {
         e.preventDefault();
 
@@ -84,31 +70,5 @@
                 window.location.href = 'index.html';
             }, 3000);
         };
-    }
-
-    function imprimirAlerta(mensaje, tipo) {
-
-        const alerta = document.querySelector('.alerta');
-
-        if (!alerta) {
-            /** Crear alerta */
-            const divMensaje = document.createElement('div');
-
-            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'max-auto', 'mt-6', 'text-center', 'border', 'alerta', 'uppercase');
-
-            if (tipo === 'error') {
-                divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
-            } else {
-                divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
-            }
-
-            divMensaje.textContent = mensaje;
-
-            formulario.appendChild(divMensaje);
-
-            setTimeout(() => {
-                divMensaje.remove();
-            }, 3000);
-        }
     }
 })();
